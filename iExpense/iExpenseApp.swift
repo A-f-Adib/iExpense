758ee38ef07f3d937aabe71f2efba9f214955254
A-f-Adib <109586107+A-f-Adib@users.noreply.github.com>
2023-11-05 02:31:22 +0600

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by A.f. Adib on 11/5/23.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
